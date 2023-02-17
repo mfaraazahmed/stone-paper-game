0090a89e6e351876a4d250a14996dd28f193cee3
Mohammed Faraaz Ahmed <91116035+mfaraazahmed@users.noreply.github.com>
2023-02-17 21:53:22 +0530

    Basic stone paper scissor game working as expected

--- app.js
+++ app.js
@@ -1,77 +1,84 @@
 let playerScore = 0;
 let computerScore = 0;
-let updatedPlayerScore = 0;
-let updatedComputerScore = 0;
-let userPick = null;
-let computerChoice = null;
-let userChoice = null;
+let round = 0;
 
-function getComputerSelection(){
-    const randChoice = Math.ceil(Math.random() * 3);
-    console.log(randChoice);
-    if(randChoice == 1){
-        computerChoice = 'rock';
-    } else if(randChoice == 2){
-        computerChoice = 'paper';
-    } else if(randChoice == 3){
-        computerChoice = 'scissor';
-    }
-    console.log(computerChoice)
-    return computerChoice;
-}
-
-function getUserSelection(){
-    document.getElementById('options').addEventListener("click", function(event){
-    if(event.target.tagName === "img"){
-        userChoice = parseInt(event.target.getAttribute("data-value"));
-        console.log(userChoice);
-    }
-    })
-    if(userChoice == 1){
-        userPick = 'rock';
-    } else if(userChoice == 2){
-        userPick = 'paper';
-    } else if(userChoice == 3){
-        userPick = 'scissor';
-    }
-    console.log(userPick)
-    return userPick;
-}
 
 function playRound(){
-    const computerSelection = getComputerSelection();
-    const userSelection = getUserSelection();
-    if(userSelection == 'rock' && computerSelection == 'sciccor' || userSelection == 'paper' && computerSelection == 'rock' || userSelection == 'scissor' && computerSelection == 'paper'){
-        return 1;
-    } else if(userSelection == 'scissor' && computerSelection == 'rock' || userSelection == 'rock' && computerSelection == 'paper' || userSelection == 'paper' && computerSelection == 'scissor'){
-        return 0;      
+
+    let computerChoice ;
+    let playerChoice ;
+
+    // Random computer selection
+    const randChoice = Math.ceil(Math.random() * 3);
+    if(randChoice == 1){
+        computerChoice = 'rock';
+    } else if( randChoice == 2){
+        computerChoice = 'paper';
     } else {
-        return null;
+        computerChoice = 'scissor';
     }
-}
 
-for(let i = 0; i < 5; i++){
-    let status = playRound();
-    if(status == 1){
-        updatedPlayerScore = updatedPlayerScore + 1;
-    } else if(status == 0){
-        updatedComputerScore = updatedComputerScore + 1;
+    // User manual selection
+    // Get the data-value attribute of the clicked image
+    const value = event.target.getAttribute('data-value');
+
+    // Set userChoice to the corresponding number
+    if (value === '1') {
+        playerChoice = 'rock';
+    } else if (value === '2') {
+        playerChoice = 'paper';
+    } else if (value === '3') {
+        playerChoice = 'scissor';
+    }
+    console.log('user choice : '+playerChoice+', cup choice : '+computerChoice);
+
+    // winning and loosing conditions
+    if(
+        playerChoice == 'rock' && computerChoice == 'scissor' ||
+        playerChoice == 'paper' && computerChoice == 'rock' ||
+        playerChoice == 'scissor' && computerChoice == 'paper'
+    ) {
+        console.log('player wins');
+        playerScore = playerScore + 1;
+    } else if (
+        playerChoice == 'scissor' && computerChoice =='rock' ||
+        playerChoice == 'rock' && computerChoice == 'paper' ||
+        playerChoice == 'paper' && computerChoice == 'scissor'
+    ) {
+        console.log('cpu wins')
+        computerScore = computerScore + 1;
     } else {
-        updatedComputerScore = updatedComputerScore;
-        updatedPlayerScore = updatedPlayerScore;
-    }  
-}
+        console.log('draw');
+    }
 
-playerScore = updatedPlayerScore;
-computerScore = updatedComputerScore;
+    console.log('player score : '+playerScore+', cpu score : '+computerScore);
+    round = round + 1;
 
-var textElement = document.createElement("p");
-if(playerScore > computerScore){
-    var displayText = 'player wins';
-} else if(computerScore > playerScore){
-    var displayText = 'player looses';
-} else {
-    var displayText = 'draw';
+    console.log(round);
+
+    if(round == 5){
+
+        // Determine the winner after 5 rounds
+        let result;
+        if (playerScore > computerScore) {
+            result = "You win!";
+        } else if (computerScore > playerScore) {
+            result = "CPU wins!";
+        } else {
+            result = "It's a tie!";
+        }
+
+        // Display the result
+        const resultDiv = document.getElementById("result");
+        resultDiv.innerText = result;
+
+
+        round = 0;
+        playerScore = 0;
+        computerScore = 0;
+    } else {
+    // Clear the result display
+    const resultDiv = document.getElementById("result");
+    resultDiv.innerText = "";
+  }
 }
-textElement.textContent = displayText;
-document.getElementById("textContainer").appendChild(textElement);
